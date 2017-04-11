4b3d78f9fcc5a37f1dbc85e01d107b3e42f6faf0
Twitter-kun <twitter@sihvi.com>
2017-04-11 15:42:45 +0100

    Proper temp folder

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,24 +5,22 @@ import java.io.File
 import com.typesafe.config.ConfigFactory
 import play.api.Logger
 
-//import com.roundeights.hasher.Digest
 import doodle.core.Image
-//import doodle.core.Image._
 import doodle.syntax._
 import doodle.jvm.Java2DCanvas._
 import doodle.backend.StandardInterpreter._
 import doodle.backend.Formats._
 import generation._
-//import play.api._
 import play.api.mvc._
-//import play.api.cache.Cache
 import play.api.Play.current
 
 import play.api.db._
 
 object Application extends Controller {
 
-  private def absolutePath(path: String = ""): String = new java.io.File(".").getCanonicalPath + "/" + path
+  private val TEMP: String = if(System.getenv("DOODLE_TEMP") != null) System.getenv("DOODLE_TEMP") else ConfigFactory.load().getString("game.temp")
+
+  private def absolutePath(path: String = ""): String = TEMP + "/" + path
 
   def index = Action {
     Ok(views.html.index(null))
@@ -31,16 +29,10 @@ object Application extends Controller {
   def hash(lat: Int, lon: Int) = Action {
     val config = ConfigFactory.load()
     val hasher: Hasher = new Hasher(lat,lon)
-    println(absolutePath("public/temp/"+hasher.getSeed()+".png"))
-    Logger.logger.debug(absolutePath("public/temp/"+hasher.getSeed()+".png"))
     if(config.getInt("game.difficulty") <= hasher.getDifficulty) {
       val doodle: Image = ImageGenerator.doodle(hasher.getSeed())
-      doodle.save[Png](absolutePath("public/temp/"+hasher.getSeed()+".png"))
+      doodle.save[Png](absolutePath(hasher.getSeed()+".png"))
       Thread.sleep(200)
-//      import java.nio.file.{Paths, Files}
-//      while(!Files.exists(Paths.get("public/temp/"+hasher.getSeed()+".png"))) {
-//        Thread.sleep(25)
-//      }
       Ok(views.html.hash(hasher.toString,hasher.getDifficulty(),true,hasher.getSeed(),hasher.getTime()))
     }
     else {
@@ -53,6 +45,11 @@ object Application extends Controller {
 //  def getImage(path: String) = Action {
 //    Ok(new File(path))
 //  }
+
+  def sendTempFile(name: String) = Action {
+    require(!name.equals(""))
+    Ok.sendFile(new java.io.File(absolutePath(name.split("/").last)))
+  }
 
   def db = Action {
     var out = ""
